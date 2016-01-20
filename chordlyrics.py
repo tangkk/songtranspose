8df6e29e12b537f23c430098fed0bf4842f31cef
tangkk <dengjunqi0632311@gmail.com>
2016-01-20 16:39:25 +0800

    update to cater to multiple time stamps per line

--- chordlyrics.py
+++ chordlyrics.py
@@ -1,5 +1,7 @@
 # this is a program to combine automatic chord transcription and lyrics
-# run it such as "python chordlyrics.py inlyricsfile inchordfile outfile"
+# run it such as "python chordlyrics.py lyricsfile chordannotationfile outfile"
+# for example run:
+# python chordlyrics.py aihenjiandan.lrc aihenjiandan.txt aihenjiandan.ch
 # written by tangkk
 
 import sys
@@ -13,32 +15,43 @@ def get_sec(s):
     ret = float(l1) * 60 + float(l21) * 1 + float(l22)*0.6 / 100
     return ret
 
+# TODO: 1. sort the lyrics in terms of secs; 2. expand lyrics when there're multiple secs
+# 3. ignore irrelavant information
 def readlyrics(inlyrics):
     title = ''
     seclist = []
     lylist = []
     f = open(inlyrics, encoding="utf8")
     for line in f:
-        sp1, sp2 = line.split(']')
-        if sp1.find('ti') == 1:
+        sps = line.split(']')
+        
+        if len(sps)==2 and sps[0].find('ti') == 1:
             _,title = sp1.split(':')
-        else:
-            sec = get_sec(sp1[1:])
-            ly = sp2
-            seclist.append(sec)
-            lylist.append(ly)
-            
+        elif len(sps)>=2 and len(sps[1])!=0:
+            # append every sec and lyrics
+            ly = sps[-1]
+            for i in range(len(sps)-1):
+                sp1 = sps[i]
+                sec = get_sec(sp1[1:])
+                seclist.append(sec)
+                lylist.append(ly)
     f.close()
-    return title, seclist, lylist
+    # sort seclist and lylist in terms of seclist
+    argsortidx = sorted(range(len(seclist)), key=lambda k: seclist[k])
+    newseclist = sorted(seclist)
+    newlylist = [lylist[i] for i in argsortidx]
     
+    return title, newseclist, newlylist
+
+# note that the annotation does not fill the end time (only start time and the chord, separated by \t)
 def readchords(inchords):
     seclist = []
     chlist = []
     f = open(inchords, encoding="utf8")
     for line in f:
-        sp1,sp2,sp3 = line.split(' ')
+        sp1,sp2 = line.split('	')
         seclist.append(float(sp1))
-        chlist.append(sp3)
+        chlist.append(sp2)
         
     f.close()
     return seclist, chlist
@@ -48,7 +61,10 @@ title, lyseclist, lylist = readlyrics(inlyrics)
 chseclist, chlist = readchords(inchords)
 
 chcount = 0
-outstr = title + '\n'
+if len(title) == 0:
+    outstr = ''
+else:
+    outstr = title + '\n'
 # the chord of this line of lyrics appears before the next line of lyrics
 for i in range(len(lyseclist)-1):
     curlysec = lyseclist[i]
@@ -56,6 +72,7 @@ for i in range(len(lyseclist)-1):
     curly = lylist[i]
     nextly = lylist[i+1]
     curline = ''
+    linebreak = 0
     while chcount < len(chseclist):
         j = chcount
         curchsec = chseclist[j]
@@ -64,13 +81,18 @@ for i in range(len(lyseclist)-1):
             chcount += 1
             curline += curch.strip()
             curline += ' | '
+            linebreak += 1
+        elif curchsec < curlysec:
+            chcount += 1
+            linebreak = 0
         else:
+            linebreak -= 1
             break
-    curline += '\n'
-    if len(curly.strip()) == 0:
-        curly = 'Intro\n'
+    if linebreak >= 0:
+        curline += '\n'
     curline += curly
-    curline += '\n'
+    # if linebreak >= 0:
+        # curline += '\n'
     outstr += curline
     
 
---
+++ testcase/aihenjiandan.ch
@@ -0,0 +1,91 @@
+歌名：愛很簡單
+C#:maj | 
+演唱：陶喆
+作詞：娃娃
+作曲：陶喆
+C#:maj | F:min | 
+忘了是怎麼開始
+A#:min | 
+也許就是對你
+C#:maj/5 | F#:maj | G#:maj | 
+有一種感覺
+C#:maj | F:min | 
+忽然間發現自己
+A#:min | 
+已深深愛上你
+C#:maj/5 | F#:maj | D#:min | 
+真的很簡單
+A#:min | F:min | 
+愛的地暗天黑都已無所謂
+C:7 | D#:min | 
+是是非非無法抉擇　喔~~
+A#:min | F:min | 
+沒有後悔為愛日夜去跟隨
+F#:maj | G#:7 | 
+那個瘋狂的人是我　喔~~
+C#:maj | G#:maj/3 | A#:min | 
+I Love You
+C#:maj/5 | F#:maj | C#:maj/3 | 
+無法不愛你 Baby
+D#:min7 | G#:maj | 
+說你也愛我　喔~~
+C#:maj | G#:maj/3 | A#:min | C#:maj/5 | 
+I Love You
+F#:maj | 
+永遠不願意 Baby
+G#:7 | C#:maj | C#:maj | 
+失去你
+F:min | 
+不可能更快樂
+A#:min | 
+只要能在一起
+C#:maj7/5 | F#:maj | 
+做什麼都可以
+G#:sus4 | C#:maj | 
+雖然
+F:min(2) | 
+世界變個不停
+A#:min | 
+用最真誠的心
+C#:maj/5 | F#:maj | D#:min | 
+讓愛變的簡單
+A#:min | F:min | 
+愛的地暗天黑都已無所謂
+C:7 | 
+是是非非無法抉擇　喔~~
+D#:min | A#:min | 
+沒有後悔為愛日夜去跟隨
+F:min | F#:maj | G#:maj | 
+那個瘋狂的人是我　喔~~
+C#:maj | G#:maj/3 | A#:min | 
+I Love You
+C#:maj/5 | F#:maj | C#:maj/3 | 
+一直在這裡　Baby
+D#:min7 | G:maj | 
+一直在愛你　喔~ya~
+C#:maj | G#:maj/3 | A#:min | C#:maj/5 | 
+I Love You (Yes I Do)
+F#:maj | G#:7 | 
+永遠都不放棄
+C#:maj | F:maj | 
+這愛你的權利
+A#:min | F#:maj | 
+如果你還有一些困惑Oh No
+A#:min | C#:maj/5 | 
+請貼著我的心傾聽
+F#:maj | C#:maj/3 | 
+聽我說著
+D#:min7 | 
+愛你
+G#:maj | C#:maj | G#:maj/3 | A#:min | C#:maj/5 | F#:maj | C#:maj/3 | D#:min7 | G#:maj | C#:maj | G#:maj/3 | A#:min | C#:maj/5 | F#:maj | G#:7 | C#:maj | G#:maj(2) | 
+(Yes I Do)
+C#:maj | G#:maj/3 | A#:min | C#:maj/5 | 
+I Love You喔(Yes I Do)
+F#:maj | C#:maj/3 | 
+一直在這裡 陪 Oh Ya
+D#:min | G#:maj | 
+一直在愛你　喔
+C#:maj | G#:maj/3 | A#:min | C#:maj/5 | 
+I Love You
+F#:maj | G#:maj | 
+永遠都不放棄
